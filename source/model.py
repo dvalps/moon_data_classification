a1a1fe01a23a44c126765f51881a5631297cefd2
Damir <dvalpy@gmail.com>
2020-12-14 20:22:00 +0100

    add files

---
+++ source/model.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+## TODO: Complete this classifier
+class SimpleNet(nn.Module):
+    
+    ## TODO: Define the init function
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        '''Defines layers of a neural network.
+           :param input_dim: Number of input features
+           :param hidden_dim: Size of hidden layer(s)
+           :param output_dim: Number of outputs
+         '''
+        super(SimpleNet, self).__init__()
+        
+        # 2 linear layers
+        self.fc1 = nn.Linear(input_dim, hidden_dim) # fully connected 1: (input_dim -> hidden)
+        self.fc2 = nn.Linear(hidden_dim, output_dim) # fully connected 2: (hidden -> output)
+
+        # droput layer
+        self.drop = nn.Dropout(0.3)
+        
+        # sigmoid layer at the output
+        self.sig = nn.Sigmoid()
+        
+    
+    ## TODO: Define the feedforward behavior of the network
+    def forward(self, x):
+        '''Feedforward behavior of the net.
+           :param x: A batch of input features
+           :return: A single, sigmoid activated value
+         '''
+        # your code, here
+        # pass through linear layers
+        x = F.relu(self.fc1(x)) # with relu activation on hidden layer
+        
+        # apply droput before the output layer
+        x = self.drop(x)
+        
+        x = self.fc2(x)  # no activation function on the output layer
+        
+        # apply sigmoid 
+        x = self.sig(x)
+        
+        return x
